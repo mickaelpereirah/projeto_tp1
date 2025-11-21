6a5618d616e1fd7e1e963120e86dae30beb92f6f
Luisa Ramos Barros <luisa.barros@s2tech.dev>
2025-11-21 11:36:56 -0300

    serviços: métodos criar senha

--- include/servicos/cadastroGerente.hpp
+++ include/servicos/cadastroGerente.hpp
@@ -1,18 +1,22 @@
 
 #ifndef CADASTRO_GERENTE_HPP_INCLUDED
-#define CADASTRO_GERENTE_HPP_INCLUDED 
+#define CADASTRO_GERENTE_HPP_INCLUDED
 
 #include "DOMINIOS.hpp"
 #include "cadastro.hpp"
 #include "gerente.hpp"
+#include "containerGerentes.hpp"
 
 using namespace std;
 
-class CadastroGerente : Cadastro {
-    public: 
-        Gerente *criar_gerente();
-        
-};
+class CadastroGerente : Cadastro
+{
+private:
+    static ContainerGerente container;
+    Gerente *criar_gerente();
 
+public:
+    bool novo_gerente();
+};
 
 #endif
--- src/servicos/cadastro.cpp
+++ src/servicos/cadastro.cpp
@@ -6,12 +6,13 @@
 
 using namespace std;
 
-
-Capacidade Cadastro::criar_capacidade() {
+Capacidade Cadastro::criar_capacidade()
+{
     Capacidade c;
     int capacidade;
     bool flag = false;
-    while (!flag) {
+    while (!flag)
+    {
         cout << "Digite a capacidade:";
         cin >> capacidade;
         try
@@ -36,5 +37,31 @@ Endereco criar_endereco();
 Nome criar_nome();
 Numero criar_numero();
 Ramal criar_ramal();
-Senha criar_senha();
+Senha criar_senha()
+{
+    Senha s;
+    string senha;
+    while (true)
+    {
+        cout << "Digite uma senha:";
+        cin >> senha;
+        try
+        {
+            s.setValor(senha);
+            break;
+        }
+        catch (const invalid_argument &e)
+        {
+            cout << "[Senha Invalida:" << senha << "]" << endl;
+            cout << "- Caracter pode ser letra (a-z, A-Z), digito (0-9) ou especial (! \" # $ % & ?)" << endl;
+            cout << "- Letra nao pode ser seguida por outra letra" << endl;
+            cout << "- Digito nao pode ser seguido por outro digito" << endl;
+            cout << "- Deve conter pelo menos uma letra minuscula (a-z)" << endl;
+            cout << "- Deve conter pelo menos uma letra maiuscula (A-Z)" << endl;
+            cout << "- Deve conter pelo menos um digito (0-9)" << endl;
+            cout << "- Deve conter pelo menos um caractere especial (! \" # $ % & ?)" << endl;
+        }
+    }
+    return s;
+}
 Telefone criar_telefone();
