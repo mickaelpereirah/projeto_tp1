2380c05a59f46ec9ebfdfb29bc289cea2bdbada7
Luisa Ramos Barros <luisa.barros@s2tech.dev>
2025-11-21 21:23:44 -0300

    tentando

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include "MenuPrincipal.hpp"
-#include "centralContainers.hpp"
 
 /**
  * @file main.cpp
  * @brief Ponto de entrada da aplicação
  * 
- * Sistema de Gestão de Hotéis
+ * Sistema de Gestão de Hotéis - 100%
  * Permite gerentes cadastrarem e gerenciarem hotéis, quartos, 
  * reservas e hóspedes através de uma interface de terminal.
  */
-
-// Inicialização dos containers estáticos
-ContainerReserva CentralContainers::containerReservas;
-ContainerHospede CentralContainers::containerHospedes;
-ContainerHotel CentralContainers::containerHoteis;
-ContainerGerente CentralContainers::containerGerentes;
-ContainerQuarto CentralContainers::containerQuartos;
 
 int main()
 {
